059d04227c1f187cdffe0253f31b35adc006bd70
Kamil Kułaga <teodozjan@gmail.com>
2013-03-28 21:13:03 +0100

    Perl6 coding guidelines continuation

---
+++ lib/real/properties/Property.pm
@@ -0,0 +1,9 @@
+# Anything that is not person
+# any( dogs, cars, houses, land, even intellectual property)
+#
+# Logic is not the thing that we expect to have because animal
+# cannot perform legal actions. Other things do not count.
+#
+class Property{
+	has PropertyId $.id;
+}

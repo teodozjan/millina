e6913eb756dff6a9f120db0d90ffb72427ab1678
Kamil Kułaga <teodozjan@gmail.com>
2013-03-28 21:38:48 +0100

    added department role

--- lib/law/LawInteractionDelegator.pm
+++ lib/law/LawInteractionDelegator.pm
@@ -1,8 +1,15 @@
- class LawInteractionDelegator{
-
+role LawDepartment{
+}
+ 
+class LawInteractionDelegator{
        my LawInteractionDelegator $instance=LawInteractionDelegator.bless(*);
+       has LawDepartment @!departments;
 
        method getInstance(){$instance}
        method new{!!!}
- 
-}
+       method addDepartment(LawDepartment $dep){
+       @!departments.push($dep);
+     }
+ }
+
+

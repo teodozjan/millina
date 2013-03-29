d7e8ce015d87d59caadcf6a8286fb36cc84fac69
Kamil Kułaga <teodozjan@gmail.com>
2013-03-29 14:02:29 +0100

    Tree renamed to uppercase + made compiling

---
+++ lib/Law/DepartmentOrchestrator.pm
@@ -0,0 +1,10 @@
+use Law::InteractionDelegator;
+
+class Law::DepartmentOrchestrator{
+
+    method createDepartments{
+    my Law::InteractionDelegator $delegator = Law::InteractionDelegator.getInstance;
+    
+    }
+
+}
